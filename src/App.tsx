76b29a045130405e12465b8a95f09e7f0b1aa4d1
Oren Elbaum <66243763+orenelbaum@users.noreply.github.com>
2022-06-02 08:29:31 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import { Component, createSignal } from 'solid-js';
-import { component } from 'babel-plugin-solid-undestructure'
+import { component } from 'babel-plugin-solid-undestructure';
 
 const Child = component(({ count: c = 100, ...other }: any) => {
-  return (
-    <button onclick={other.increment}>{c}</button>
-  );
+  return <button onclick={other.increment}>{c}</button>;
 });
 
-
 const App: Component = () => {
-  const [count, setCount] = createSignal()
-  const increment = () => setCount(count() ? count() + 1 : 1);
+  const [count, setCount] = createSignal<number | undefined>();
+  const increment = () => setCount(count() ? count()! + 1 : 1);
 
-  return (
-    <Child count={count()} {...{increment}}/>
-  );
+  return <Child count={count()} {...{ increment }} />;
 };
 
-export default App
+export default App;
